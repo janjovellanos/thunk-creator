6d2523f8b63004e640df7f61090bb0ec41535409
Jan <janjovellanos@gmail.com>
2022-06-21 19:40:02 -0700

    progress

--- frontend/src/store/articleReducer.js
+++ frontend/src/store/articleReducer.js
@@ -3,10 +3,10 @@ import articles from '../data/data.json';
 const LOAD_ARTICLES = 'article/loadArticles';
 const ADD_ARTICLE = 'article/addArticle';
 
-export const loadArticles = () => {
+export const loadArticles = (articleData) => {
   return {
     type: LOAD_ARTICLES,
-    articles
+    articles: articleData || articles
   };
 };
 
@@ -16,6 +16,26 @@ export const addArticle = (article) => {
     article
   };
 };
+
+export const fetchArticles = () => async (dispatch) => {
+  const response = await fetch('/api/articles'); //response
+  const articles = await response.json(); //parsed response
+
+  dispatch(loadArticles(articles));
+};
+
+export const writeArticle = (data) => async (dispatch) => {
+  const res = await fetch('/api/articles', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+
+  if (res.ok) {
+    const parsedRes = await res.json();
+    dispatch(addArticle(parsedRes));
+  }
+}
 
 const initialState = { entries: [], isLoading: true };
 
